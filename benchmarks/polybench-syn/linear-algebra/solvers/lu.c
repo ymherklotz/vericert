c1ed2726725dc60f5c96d7aa7952b26136388bdd
Nadesh Ramanathan <nadeshramanathan88@gmail.com>
2020-11-17 11:28:26 +0000

    updated lu

--- benchmarks/polybench-syn/linear-algebra/solvers/lu.c
+++ benchmarks/polybench-syn/linear-algebra/solvers/lu.c
@@ -13,7 +13,6 @@
 //#include <unistd.h>
 //#include <string.h>
 //#include <math.h>
-#include <stdio.h>
 
 #include "../../include/misc.h"
 
@@ -25,8 +24,9 @@
 
 static
 void init_array (int n,
-                 int A[40][40])
+   int A[40][40])
 {
+  int ONE = 1;
   int i, j;
 
   for (i = 0; i < n; plus(i))
@@ -38,8 +38,6 @@ void init_array (int n,
       }
       A[i][i] = 1;
     }
-    A[i][i] = 1;
-  }
 
 
 
@@ -51,40 +49,39 @@ void init_array (int n,
   for (t = 0; t < n; plus(t))
     for (r = 0; r < n; plus(r))
       for (s = 0; s < n; plus(s))
-        B[r][s] += A[r][t] * A[s][t];
-  for (r = 0; r < n; plus(r))
-    for (s = 0; s < n; plus(s))
-      A[r][s] = B[r][s];
+ B[r][s] += A[r][t] * A[s][t];
+    for (r = 0; r < n; plus(r))
+      for (s = 0; s < n; plus(s))
+ A[r][s] = B[r][s];
   //free((void*)B);;
 
 }
 
-
 static
 void kernel_lu(int n,
-               int A[ 40][40])
+        int A[ 40][40])
 {
   int i, j, k;
   int ONE = 1;
 
-  for (i = 0; i < n; plus(i)) {
+ for (i = 0; i < n; plus(i)) {
     for (j = 0; j <i; plus(j)) {
-      for (k = 0; k < j; plus(k)) {
-        A[i][j] -= A[i][k] * A[k][j];
-      }
-      A[i][j] = A[i][j] / A[j][j];
+       for (k = 0; k < j; plus(k)) {
+          A[i][j] -= A[i][k] * A[k][j];
+       }
+        A[i][j] = sdivider(A[i][j], A[j][j]);
     }
-    for (j = i; j < n; plus(j)) {
-      for (k = 0; k < i; plus(k)) {
-        A[i][j] -= A[i][k] * A[k][j];
-      }
+   for (j = i; j < n; plus(j)) {
+       for (k = 0; k < i; plus(k)) {
+          A[i][j] -= A[i][k] * A[k][j];
+       }
     }
   }
 }
 
 static
 int check_array(int n,
-                int A[40][40])
+   int A[40][40])
 {
   int res = 0;
   int i, j;
@@ -115,6 +112,7 @@ int main()
   kernel_lu (n, A);
 
   return check_array(n, A);
+  return 0;
 
   //free((void*)A);;
 }
