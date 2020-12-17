6109d3d4af3efed0dddaa6bf1aa532dd83d8d465
Yann Herklotz <git@yannherklotz.com>
2020-12-17 10:04:55 +0000

    Modify array test case to be suitable for loop pipelining

--- test/array.c
+++ test/array.c
@@ -1,7 +1,9 @@
 int main() {
-    int x[3] = {1, 2, 3};
+    int x[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = 0;
-    for (int i = 0; i < 3; i++)
-        sum = x[i];
+    for (int i = 0; i < 10; i++) {
+        sum = sum + x[i];
+        x[i] = sum;
+    }
     return sum;
 }
