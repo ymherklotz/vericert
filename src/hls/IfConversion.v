ab62ee5a82fcde31bbb45e2ddd66c1917cd57383
Yann Herklotz <git@yannherklotz.com>
2021-02-02 12:55:53 +0000

    Add if conversion pass

---
+++ src/hls/IfConversion.v
@@ -0,0 +1,32 @@
+(*
+ * Vericert: Verified high-level synthesis.
+ * Copyright (C) 2021 Yann Herklotz <yann@yannherklotz.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *)
+
+Require Import compcert.common.AST.
+Require compcert.common.Errors.
+Require compcert.common.Globalenvs.
+Require compcert.lib.Integers.
+Require Import compcert.lib.Maps.
+
+(*|
+=============
+If conversion
+=============
+
+This conversion is a verified conversion from RTLBlock back to itself, which performs if-conversion
+on basic blocks to make basic blocks larger.
+|*)
