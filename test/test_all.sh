890f8d7619364b725417bf1694e6192312052f74
Michalis Pardalos <m.pardalos@gmail.com>
2021-03-01 11:44:23 +0000

    Turn off -fschedule in test runner. Breaks sharing

--- test/test_all.sh
+++ test/test_all.sh
@@ -30,7 +30,7 @@ for cfile in $test_dir/*.c; do
     gcc -o $outbase.gcc $cfile >/dev/null 2>&1
     $outbase.gcc
     expected=$?
-    ./bin/vericert -fschedule -drtl -o $outbase.v $cfile >/dev/null 2>&1
+    ./bin/vericert -drtl -o $outbase.v $cfile >/dev/null 2>&1
     if [[ ! -f $outbase.v ]]; then
         echo "ERROR"
         continue
