0c021173b3efb1310370de4b2a6f5444c745022f
Yann Herklotz <git@yannherklotz.com>
2021-07-24 16:09:09 +0200

    Use main instead of top for synthesising Vericert designs

--- scripts/synth.tcl
+++ scripts/synth.tcl
@@ -77,7 +77,7 @@ proc dump_statistics {  } {
 set outputDir .
 create_project -in_memory -part xc7z020clg484-1 -force
 read_verilog main.v
-synth_design -mode out_of_context -no_iobuf -top top -part xc7z020clg484-1
+synth_design -mode out_of_context -no_iobuf -top main -part xc7z020clg484-1
 write_checkpoint -force $outputDir/post_synth.dcp
 report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
 report_utilization -file $outputDir/post_synth_util.rpt
