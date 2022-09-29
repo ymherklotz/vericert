4ba9568762cecd2bda449d19fed45cfd002fcdbe
Yann Herklotz <git@yannherklotz.com>
2022-09-29 19:01:21 +0100

    Removing duplication in flake.nix file

--- flake.nix
+++ flake.nix
@@ -4,53 +4,19 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
 
   outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      ncoq = pkgs.coq_8_14;
-      ncoqPackages = pkgs.coqPackages_8_14;
-      dpkgs = nixpkgs.legacyPackages.x86_64-darwin;
-      dncoq = dpkgs.coq_8_14;
-      dncoqPackages = dpkgs.coqPackages_8_14;
+    let vericertDevPackages = pkgs:
+          let
+            ncoq = pkgs.coq_8_14;
+            ncoqPackages = pkgs.coqPackages_8_14;
+          in
+            pkgs.mkShell {
+              buildInputs = with pkgs;
+                [ ncoq ncoq.ocaml ncoqPackages.serapi dune_3 gcc python3 ]
+                ++ (with ncoq.ocamlPackages; [ findlib menhir menhirLib ocamlgraph ocp-indent utop ])
+                ++ (with python3Packages; [ alectryon sphinx_rtd_theme ]);
+            };
     in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [ ncoq
-            dune_3
-            gcc
-            ncoq.ocaml
-            ncoq.ocamlPackages.findlib
-            ncoq.ocamlPackages.menhir
-            ncoq.ocamlPackages.ocamlgraph
-            ncoq.ocamlPackages.menhirLib
-
-            ncoq.ocamlPackages.ocp-indent
-            ncoq.ocamlPackages.utop
-
-            ncoqPackages.serapi
-            python3
-            python3Packages.alectryon
-            python3Packages.sphinx_rtd_theme
-          ];
-      };
-      devShell.x86_64-darwin = dpkgs.mkShell {
-        buildInputs = with dpkgs;
-          [ dncoq
-            dune_3
-            gcc
-            dncoq.ocaml
-            dncoq.ocamlPackages.findlib
-            dncoq.ocamlPackages.menhir
-            dncoq.ocamlPackages.ocamlgraph
-            dncoq.ocamlPackages.menhirLib
-
-            dncoq.ocamlPackages.ocp-indent
-            dncoq.ocamlPackages.utop
-
-            dncoqPackages.serapi
-            python3
-            python3Packages.alectryon
-            python3Packages.sphinx_rtd_theme
-          ];
-      };
+      devShell.x86_64-linux = vericertDevPackages nixpkgs.legacyPackages.x86_64-linux;
+      devShell.x86_64-darwin = vericertDevPackages nixpkgs.legacyPackages.x86_64-darwin;
     };
 }
