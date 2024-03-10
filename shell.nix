f28bb3fb6ccfc8eb5dc1c494cd4db111dd482f09
Yann Herklotz <git@yannherklotz.com>
2024-03-10 22:16:54 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
-with import <nixpkgs> {};
-
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8dd8bd8be74879f9f7919b16a4cb5ab2a75f18e5.tar.gz") {};
+let
+  ncoq = coq_8_13;
+  ncoqPackages = coqPackages_8_13;
+in
 mkShell {
-  buildInputs = (import ./.).buildInputs ++ [ ocamlPackages.ocp-indent
-                                              ocamlPackages.merlin ocamlPackages.utop
-                                              # (import ./.)
-                                            ];
+  buildInputs = [ ocamlPackages.ocp-indent
+                  ocamlPackages.merlin ocamlPackages.utop
+                  # (import ./.)
+                ] ++ 
+                [ ncoq dune_2 gcc
+                  ncoq.ocaml ncoq.ocamlPackages.findlib ncoq.ocamlPackages.menhir
+                  ncoq.ocamlPackages.ocamlgraph
+                ];
 }
