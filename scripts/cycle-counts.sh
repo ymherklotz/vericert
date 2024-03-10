896d69ebdaacc41e88f52d726d6c2bbcbd52b539
Yann Herklotz <git@yannherklotz.com>
2024-03-10 22:43:51 +0100

    Add cycle-counts

---
+++ scripts/cycle-counts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "##################################################"
+echo "# Compiling benchmark: vericert-hyperblock"
+echo "##################################################"
+VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule -fif-conv" make -j
+
+echo "##################################################"
+echo "# Running benchmark: vericert-hyperblock"
+echo "##################################################"
+run-vericert.sh
+mv exec.csv vericert-hyperblock-cycles.csv
+make clean
+
+echo "##################################################"
+echo "# Compiling benchmark: vericert-list-scheduling"
+echo "##################################################"
+VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule" make -j
+
+echo "##################################################"
+echo "# Running benchmark: vericert-list-scheduling"
+echo "##################################################"
+run-vericert.sh
+mv exec.csv vericert-list-scheduling-cycles.csv
+make clean
+
+echo "##################################################"
+echo "# Compiling benchmark: vericert-original"
+echo "##################################################"
+VERICERT=vericert-original VERICERT_OPTS="-DSYNTHESIS" make -j
+
+echo "##################################################"
+echo "# Compiling benchmark: vericert-original"
+echo "##################################################"
+run-vericert.sh
+mv exec.csv vericert-original-cycles.csv
+make clean
