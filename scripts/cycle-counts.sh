96f79c1ed213c665d499094bd534e5a3a5ea970d
Yann Herklotz <git@yannherklotz.com>
2024-03-11 17:47:48 +0100

    Add run-bambu.sh

--- scripts/cycle-counts.sh
+++ scripts/cycle-counts.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+target=""
+if [[ ! -z $1 ]]; then
+    mv benchmark-list-master benchmark-list-master.bck
+    echo "$1" >benchmark-list-master
+    target="$1"
+else
+    git checkout benchmark-list-master
+fi
+
+echo "##################################################"
+echo "# Compiling and running benchmark: bambu-opt"
+echo "##################################################"
+cat benchmark-list-master | xargs -P8 -n1 run-bambu.sh opt
+mv exec.csv bambu-opt-cycles.csv
+git clean -dfx -e '*.csv'
+
+echo "##################################################"
+echo "# Compiling and running benchmark: bambu-opt"
+echo "##################################################"
+cat benchmark-list-master | xargs -P8 -n1 run-bambu.sh noopt
+mv exec.csv bambu-noopt-cycles.csv
+git clean -dfx -e '*.csv'
+
 echo "##################################################"
 echo "# Compiling benchmark: vericert-hyperblock"
 echo "##################################################"
-VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule -fif-conv" make -j
+VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule -fif-conv" make -j $target
 
 echo "##################################################"
 echo "# Running benchmark: vericert-hyperblock"
@@ -15,7 +38,7 @@ git clean -dfx -e '*.csv'
 echo "##################################################"
 echo "# Compiling benchmark: vericert-list-scheduling"
 echo "##################################################"
-VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule" make -j
+VERICERT=vericert VERICERT_OPTS="-DSYNTHESIS -fschedule" make -j $target
 
 echo "##################################################"
 echo "# Running benchmark: vericert-list-scheduling"
@@ -27,11 +50,16 @@ git clean -dfx -e '*.csv'
 echo "##################################################"
 echo "# Compiling benchmark: vericert-original"
 echo "##################################################"
-VERICERT=vericert-original VERICERT_OPTS="-DSYNTHESIS" make -j
+VERICERT=vericert-original VERICERT_OPTS="-DSYNTHESIS" make -j $target
 
 echo "##################################################"
 echo "# Compiling benchmark: vericert-original"
 echo "##################################################"
 run-vericert.sh
 mv exec.csv vericert-original-cycles.csv
+
+if [[ ! -z $1 ]]; then
+    mv benchmark-list-master.bck benchmark-list-master
+fi
+
 git clean -dfx -e '*.csv'
---
+++ scripts/run-bambu.sh
@@ -0,0 +1,26 @@
+opt=$1
+bench=$2
+base=$(pwd)
+path=$(dirname $bench)
+name=$(basename $bench)
+
+cd $path
+
+mkdir $name
+cp $name.c $name/.
+cd $name
+
+sed -i 's/main/main_top/' $name.c
+sed -i 's:include/misc.h:../include/misc.h:' $name.c
+
+if [[ "$opt" = "opt" ]]; then
+    bambu --simulate --top-fname=main_top $name.c 2>/dev/null
+else
+    bambu -fno-if-conversion -O0 --do-not-chain-memories --do-not-use-asynchronous-memories --clock-period=10 --top-fname=main_top --no-chaining --simulate $name.c 2>/dev/null
+fi
+
+cycles=$(./simulate_main_top.sh | sed -n -E -e 's/^.*\s+([0-9]+)\s+cycles.*$/\1/ p')
+
+echo $name,$cycles >>$base/exec.csv
+
+cd $base
