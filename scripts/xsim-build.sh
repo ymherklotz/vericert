17ae6b993eb5ecda7c4d917d976b5c1c5c3e4e67
Yann Herklotz <git@yannherklotz.com>
2025-12-24 22:51:36 +0100

    Update xsim script

--- scripts/xsim-build.sh
+++ scripts/xsim-build.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 export PATH=/opt/vitis/Vivado/2023.2/bin:$PATH
 
-xvhdl /home/ymh/projects/vericert/ip/float/float.vhd
+xvhdl $SCRIPT_DIR/../ip/float/float.vhd
 xvlog -sv $1
 
 xelab testbench -L unisims_ver -L unimacro_ver -L xilinxcorelib_ver -L secureip -L xpm
